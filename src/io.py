3f7d73ca4c987e0c0f29b732542a7625c0bf9207
drew-dectech <drew@december.tech>
2022-12-27 10:20:59 -0800

    Update io.py

--- src/io.py
+++ src/io.py
@@ -15,28 +15,28 @@ def parse_input() -> (list, tuple, int, bool, str, str, str, str):
         expected_string = f'{args[0]} ignore_branches prefixes_to_delete last_commit_age_days dry_run_yes_no github_token github_repo github_base_url, ignore_suffix'
         raise RuntimeError(f'Incorrect input: {input_string}. Expected: {expected_string}')
 
-    prefixes_raw: str = args[2]
-    prefixes_to_delete = prefixes_raw.split(',')
-    if prefixes_to_delete == ['']:
-        raise RuntimeWarning('There are no prefixes to search.')
-
     branches_raw: str = args[1]
     ignore_branches = branches_raw.split(',')
     if ignore_branches == ['']:
         ignore_branches = []
 
-    last_commit_age_days = int(args[2])
+    prefixes_raw: str = args[2]
+    prefixes_to_delete = prefixes_raw.split(',')
+    if prefixes_to_delete == ['']:
+        raise RuntimeWarning('There are no prefixes to search.')
+
+    last_commit_age_days = int(args[3])
 
     # Dry run can only be either `true` or `false`, as strings due to github actions input limitations
-    dry_run = False if args[3] == 'no' else True
+    dry_run = False if args[4] == 'no' else True
 
-    github_token = args[4]
+    github_token = args[5]
 
     github_repo = getenv('GITHUB_REPOSITORY')
 
-    github_base_url = args[5]
+    github_base_url = args[6]
 
-    ignore_suffix = args[6]
+    ignore_suffix = args[7]
 
     return ignore_branches, prefixes_to_delete, last_commit_age_days, dry_run, github_token, github_base_url, github_repo, ignore_suffix
 
